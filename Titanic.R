d00d0ce7ce58bae8629f45e9719d4b12c4b3d528
Fernando Barbeiro <fernandoabcampos@gmail.com>
2018-04-09 22:53:07 +0200

    Titanic data set

---
+++ Titanic.R
@@ -0,0 +1,43 @@
+library(plotrix) # pie chart
+
+
+# Para definir el algoritmo de lectura read.csv o read.csv2
+L <- readLines("titanic.csv", n = 1) 
+if (grepl(",", L)) print("English format")
+
+df <- read.csv("titanic.csv")
+head(df)
+
+#Mirando los nombres de columnas del dataframe y despues los tipos de variables estadisticas
+colnames(df)
+sapply(df,class)
+str(df)
+summary(df)
+
+
+not_adult <- df[!(df$AGE == "Adulto"), ]
+
+
+
+sexo <- table(df$SEX)
+lbls <- paste(names(sexo), "\n", sexo, sep="")
+pie3D(sexo
+      , radius=0.9
+      , labelcex= 0.8
+      , labels = lbls
+      , explode=0.0
+      , main="Sexo de los pasajeros"
+      , col = rainbow(length(lbls)
+      ))
+
+no_supervivientes <- df[(df$SURVIVED == "Muere"), ]
+sexo_muertos <- table(no_supervivientes$SEX)
+lbls <- paste(names(sexo_muertos), "\n", sexo_muertos, sep="")
+pie3D(sexo_muertos
+      , radius=0.9
+      , labelcex= 0.8
+      , labels = lbls
+      , explode=0.0
+      , main="Deaths (amount per sex)"
+      , col = rainbow(length(lbls)
+      ))
